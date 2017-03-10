0996a0430a041cb905aaaaadefdeca348e80e408
Michael Thomas (malinka) <malinka@entropy-development.com>
2017-03-10 00:59:42 -0500

    windows

--- win/mklinks.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-rmdir Entropy\include\Entropy
-rmdir Hecate\include\Entropy\Hecate
-mklink /d Entropy\include\Entropy ..\..\..\..\Entropy\src
-mklink /d Hecate\include\Entropy\Hecate ..\..\..\..\..\Hecate\src
