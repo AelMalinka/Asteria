386cb712963dd62d581ca7f38e7f3061619bf630
Michael Thomas (malinka) <malinka@entropy-development.com>
2018-01-04 20:31:20 -0500

    add Logger

---
+++ src/Logger.cc
@@ -0,0 +1,16 @@
+/*	Copyright 2018 (c) Michael Thomas (malinka) <malinka@entropy-development.com>
+	Distributed under the terms of the GNU Affero General Public License v3
+*/
+
+#include "Exception.hh"
+
+using namespace Entropy;
+
+namespace Entropy
+{
+	namespace Asteria
+	{
+		Log::Logger Log("Asteria");
+	}
+}
+
