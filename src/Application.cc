0ad4f1d1ba2eb1b83866bf4121a8cf753f04589a
Michael Thomas (malinka) <malinka@entropy-development.com>
2018-01-27 21:02:48 -0500

    use generated cave level
    TODO: fill in mobs randomly not in calcualted places
    TODO: guarntee access to all of map
    TODO: improve performance (don't draw offscreen tiles)

--- src/Application.cc
+++ src/Application.cc
@@ -14,6 +14,7 @@
 
 #include <Entropy/Mnemosyne/Resources/Texture.hh>
 #include <Entropy/Mnemosyne/Resources/Json.hh>
+#include <cassert>
 
 using namespace std;
 using namespace Entropy::Asteria;
@@ -106,31 +107,34 @@ void Application::Restart()
 	_weapon = make_shared<Weapon>("Basic Sword"s, sword.shared(), Weapon::Type::Sword, None);
 	_armor = make_shared<Armor>("Basic Light Armor"s, armor.shared(), Armor::Type::Light, None);
 
-	// 2018-01-11 AMR NOTE: calculated with the super scientific method of trial and error
-	auto height = 17;
-	auto width = 29;
+	Cave c(100, 100, floor.shared(), wall.shared());
+	c();
 
-	const Tile Wall(wall.shared(), true);
-	const Tile Floor(floor.shared());
+	auto height = c.Height();
+	auto width = c.Width();
 
-	vector<vector<Tile>> v;
-
-	for(auto x = 0; x < width; x++) {
-		vector<Tile> t;
-		for(auto y = 0; y < height; y++) {
-			if(x == 0 || x == width  - 1|| y == 0 || y == height - 1)
-				t.push_back(Wall);
-			else
-				t.push_back(Floor);
-		}
-		v.push_back(t);
-	}
-
-	_map = make_shared<Map>(move(v));
+	_map = make_shared<Map>(c.Tiles());
 	_player = make_shared<Character>(player.shared(), 10, 10, 10, 10, 10, 10);
 
-	_player->Translate(Vertex(width / 2, height / 2, 0));
-	(*_map)[width / 2 - width / 4][height / 2].setActor(make_shared<Character>(_enemy.shared(), 0, 0, 1, 0, 0, 0));
+	auto px = width / 2;
+	auto py = height / 2;
+
+	auto ex = width / 2 - width / 4;
+	auto ey = height / 2;
+
+	while((*_map)[px][py].isWall()) {
+		++px;
+	}
+
+	while((*_map)[ex][ey].isWall()) {
+		++ex;
+	}
+
+	assert(px < width);
+	assert(ex < width);
+
+	_player->Translate(Vertex(px, py, 0));
+	(*_map)[ex][ey].setActor(make_shared<Character>(_enemy.shared(), 0, 0, 1, 0, 0, 0));
 
 	_menu = addMode(make_shared<Modes::Menu>(*this));
 	_world = addMode(make_shared<Modes::World>(*this, _player, _map));
@@ -169,10 +173,27 @@ void Application::Win(const shared_ptr<Character> &w)
 
 		(*_map)[_player->Position().x][_player->Position().y].setActor(shared_ptr<Character>());
 
-		if(_player->Position().x == width / 2 - width / 4 && _player->Position().y == height / 2) {
-			(*_map)[width / 2 + width / 4][height / 2].setActor(make_shared<Character>(_enemy.shared(), 0, 0, 1, 0, 0, 0));
+		auto ex1 = width / 2 - width / 4;
+		auto ey1 = height / 2;
+
+		auto ex2 = width / 2 + width / 4;
+		auto ey2 = height / 2;
+
+		while((*_map)[ex1][ey1].isWall()) {
+			++ex1;
+		}
+
+		while((*_map)[ex2][ey2].isWall()) {
+			++ex2;
+		}
+
+		assert(ex1 < _map->Width());
+		assert(ex2 < _map->Width());
+
+		if(_player->Position().x == ex1 && _player->Position().y == ey1) {
+			(*_map)[ex2][ey2].setActor(make_shared<Character>(_enemy.shared(), 0, 0, 1, 0, 0, 0));
 		} else {
-			(*_map)[width / 2 - width / 4][height / 2].setActor(make_shared<Character>(_enemy.shared(), 0, 0, 1, 0, 0, 0));
+			(*_map)[ex1][ey1].setActor(make_shared<Character>(_enemy.shared(), 0, 0, 1, 0, 0, 0));
 		}
 
 		setMode(_world);
